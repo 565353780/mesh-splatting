63f657d5eaed953c73953447b99d0d09d705b9b0
chLi <565353780@qq.com>
2026-01-06 13:10:44 +0800

    create setup bash files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# mesh-splatting
+# Mesh Splatting
+
+## Setup
+
+```bash
+conda create -n ms python=3.10
+conda activate ms
+./setup.sh
+```
+
+## Run
+
+```bash
+./train.sh
+```
+
+## Enjoy it~
---
+++ dev_setup.sh
@@ -0,0 +1,22 @@
+cd ..
+git clone --depth 1 https://github.com/rahul-goel/fused-ssim.git
+
+pip install ninja pybind11
+
+pip3 install torch torchvision \
+  --index-url https://download.pytorch.org/whl/cu128
+
+pip install lpips plyfile mediapy opencv-python matplotlib \
+  tqdm trimesh mmengine mmcv timm
+
+cd fused-ssim
+python setup.py install
+
+cd ../mesh-splatting/submodules/diff-triangle-mesh-rasterization
+python setup.py install
+
+cd ../simple-knn
+python setup.py install
+
+cd ../effrdel
+python setup.py install
