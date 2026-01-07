043cc7b790efeed27f4f3477e38a964793336c26
chLi <565353780@qq.com>
2026-01-07 14:45:09 +0800

    create train with mesh algo

---
+++ train_with_mesh.sh
@@ -0,0 +1,16 @@
+DATA_FOLDER=$HOME/chLi/Dataset/MM/Match/gold_dragon
+ITERATIONS=3000
+
+LIBRARY_PATH=/usr/local/cuda-12.4/targets/x86_64-linux/lib/stubs:$LIBRARY_PATH \
+  CUDA_VISIBLE_DEVICES=3 \
+  python train.py \
+  -s ${DATA_FOLDER}/colmap/ \
+  -m ${DATA_FOLDER}/meshsplatting/ \
+  --mesh_file ${DATA_FOLDER}/gold_dragon.glb \
+  --indoor \
+  --eval \
+  --iterations ${ITERATIONS}
+
+python create_ply.py \
+  ${DATA_FOLDER}/meshsplatting/point_cloud/iteration_${ITERATIONS}/ \
+  --out ${DATA_FOLDER}/meshsplatting/point_cloud/iteration_${ITERATIONS}/mesh.ply
